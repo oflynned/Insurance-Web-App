15b4db7cfd961af3ff04af5f9eb9653f22f1930d
Edmond O'Flynn <oflynned@tcd.ie>
2019-08-09 15:25:36 +0200

    add private route to app.js router

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 
+import "./App.css";
+
+import PrivateRoute from "./components/authentication/privateRoute";
+
 import Login from "./screens/login";
+import Quote from "./screens/quote";
 import Plans from "./screens/plans";
 import PageNotFound from "./screens/pageNotFound";
-
-import "./App.css";
-import Quote from "./screens/quote";
 
 function App () {
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route path="/login" component={Login}/>
-          <Route path="/quote" component={Quote}/>
-          <Route path="/plans" component={Plans}/>
-          <Route path="/404" component={PageNotFound}/>
+          <Route exact path="/" component={Login}/>
+          <Route exact path="/login" component={Login}/>
+          <PrivateRoute exact path="/quote" component={Quote}/>
+          <PrivateRoute exact path="/plans" component={Plans}/>
+          <Route exact path="/404" component={PageNotFound}/>
           <Redirect to="/404"/>
         </Switch>
       </div>
