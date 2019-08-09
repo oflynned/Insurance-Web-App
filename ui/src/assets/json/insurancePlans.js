ebc0cced898033cb0f1e87efcdc88f60215bef56
Edmond O'Flynn <oflynned@tcd.ie>
2019-08-09 16:49:41 +0200

    remove annual quote values from json assets

--- ui/src/assets/json/insurancePlans.js
+++ ui/src/assets/json/insurancePlans.js
@@ -1,28 +1,23 @@
-const plans = [
-  {
-    title: "Global",
-    annualCost: 78.30,
-    travelDurationDays: 90,
-    medicalReimbursement: 1000000,
-    personalAssistanceAbroad: 5000,
-    travelAssistanceAbroad: 1000,
-    duration: {
-      length: 1,
-      unit: "year"
-    }
-  },
-  {
-    title: "Universe",
-    annualCost: 114.71,
-    travelDurationDays: 180,
-    medicalReimbursement: 3000000,
-    personalAssistanceAbroad: 10000,
-    travelAssistanceAbroad: 2500,
-    duration: {
-      length: 1,
-      unit: "year"
-    }
+export const GLOBAL_PLAN = {
+  title: "Global",
+  travelDurationDays: 90,
+  medicalReimbursement: 1000000,
+  personalAssistanceAbroad: 5000,
+  travelAssistanceAbroad: 1000,
+  duration: {
+    length: 1,
+    unit: "year"
   }
-];
+};
 
-export default plans;
+export const UNIVERSAL_PLAN = {
+  title: "Universe",
+  travelDurationDays: 180,
+  medicalReimbursement: 3000000,
+  personalAssistanceAbroad: 10000,
+  travelAssistanceAbroad: 2500,
+  duration: {
+    length: 1,
+    unit: "year"
+  }
+};
